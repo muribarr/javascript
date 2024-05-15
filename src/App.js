554f13bb3a110c5e54253ddb9fd87f32166ac644
muribarr <barmurilo230@gmail.com>
2024-05-14 22:16:29 -0300

    new:meu primeiro projeto em javascripy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css"
+
+
+
+export default function App() {
+  return (
+    <body>
+      <div className="card">
+        <h1>React</h1>
+        <p>Front-End JavaScripy</p>
+      </div>
+    </body>
+  );
+}
